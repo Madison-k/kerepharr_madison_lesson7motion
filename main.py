f53356dcb1df4ea6d06da632a8e0d825758b7b85
Madison-k <72760208+Madison-k@users.noreply.github.com>
2021-01-27 10:46:09 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+while True:
+    if input.rotation(Rotation.PITCH):
+        light.set_all(light.rgb(250, 0, 0))
---
+++ main.ts
@@ -0,0 +1,6 @@
+while (true) {
+    if (input.rotation(Rotation.Pitch)) {
+        light.setAll(light.rgb(250, 0, 0))
+    }
+    
+}
